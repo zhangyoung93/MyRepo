6c9d0af44cd4cd198b3d974edc587dd1d35a12c4
zhangyang7 <zhangyang7@asiainfo.com>
2020-07-27 22:40:13 +0800

    commit src/

---
+++ src/main/java/com/zy/demo/JavaTestDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zy.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zy/demo/JavaTestDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

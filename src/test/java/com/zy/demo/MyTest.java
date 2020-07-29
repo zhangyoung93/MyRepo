0949faae89bb9413a211edc64a78dae2f935ba75
zhangyoung93 <414218039@qq.com>
2020-07-29 23:18:03 +0800

    main test commit

---
+++ src/test/java/com/zy/demo/MyTest.java
@@ -0,0 +1,10 @@
+package com.zy.demo;
+
+public class MyTest {
+
+    public static void main(String[] args){
+        long start = System.currentTimeMillis();
+        long end = System.currentTimeMillis();
+        System.out.println("程序执行耗费时间="+(end-start)+"ms");
+    }
+}

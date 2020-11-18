b20964ef94323656bf3a7ff0d40733ba38edbcbb
zhangyoung93 <414218039@qq.com>
2020-11-18 14:19:03 +0800

    class adapter

---
+++ src/main/java/com/zy/demo/design/ClassAdapter.java
@@ -0,0 +1,44 @@
+package com.zy.demo.design;
+
+/**
+ * 类适配器
+ * @author zy
+ */
+public class ClassAdapter {
+}
+
+/**
+ * 目标类
+ */
+interface Target{
+    /**
+     * 目标方法
+     */
+    void targetMethod();
+}
+
+/**
+ * 源类
+ */
+class Source{
+    /**
+     * 源方法
+     */
+    protected void sourceMethod(){
+
+    }
+}
+
+/**
+ * 适配器
+ */
+class AdapterClass extends Source implements Target{
+
+    /**
+     * 适配目标方法
+     */
+    @Override
+    public void targetMethod() {
+        super.sourceMethod();
+    }
+}

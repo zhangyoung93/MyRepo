348690ee4317acf5deb34f5743ece5f94fe0df2a
zhangyoung93 <414218039@qq.com>
2020-11-24 11:30:02 +0800

    service loader #2

--- src/main/java/com/zy/demo/config/CustomerServiceLoader.java
+++ src/main/java/com/zy/demo/config/CustomerServiceLoader.java
@@ -10,9 +10,13 @@ import java.util.ServiceLoader;
  */
 public class CustomerServiceLoader {
     public static void main(String[] args){
+        //加载指定接口的ServiceLoader
         ServiceLoader<TestService> serviceLoader = ServiceLoader.load(TestService.class);
+        //遍历接口实现类
         for(TestService testService : serviceLoader){
+            //获取接口实现类名称
             System.out.println(testService.getClass().getName());
+            //执行接口方法
             testService.doTest();
         }
     }
